d83d4f0252c2826851fc998088af931ad8f4c31d
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:05:51 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert ("Mi archivo está bien linkeado")
+
+const minutos = prompt ('Ingresa un valor en minutos');
+
+const resultado = minutos * 60;
+
+alert ('El valor ingresado en minutos: ' + minutos +', equivale a: '+ resultado + ' segundos.');
+
+console.log (alert);
